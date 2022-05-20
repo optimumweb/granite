5689512e07cc22a81e4ece690bec2c30e175bcd2
Jonathan Roy <jroy66@gmail.com>
2022-05-20 11:45:04 -0400

    default page and post

---
+++ views/page.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+
+@section('content')
+    <h2>{{ $page }}</h2>
+@endsection
---
+++ views/post.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+
+@section('content')
+    <h2>{{ $post }}</h2>
+@endsection
