9824882714a09520355bba0a3fdc4d5e5530986d
Jonathan Roy <jroy66@gmail.com>
2022-05-26 10:51:06 -0400

    dev

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.default')
 
 @section('content')
-    <h2>{{ $entry->title }}</h2>
+    <article class="entry post">
+        <h2 class="entry-title">
+            {!! $entry->title !!}
+        </h2>
+
+        <div class="entry-meta">
+            <span class="entry-author">
+                {{ $entry->author }}
+            </span>
+
+            <span class="entry-published-at">
+                {{ $entry->published_at }}
+            </span>
+        </div>
+
+        <div class="entry-content">
+            {!! $entry->content !!}
+        </div>
+    </article>
 @endsection
