1255ae607c11aeeb476545f3587c3d4cf7046aa1
Jonathan Roy <jroy66@gmail.com>
2022-05-27 15:34:42 -0400

    design theme

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -12,11 +12,33 @@
     </head>
     <body>
         <header id="site-header">
-            <h1>{{ config('site.name') }}</h1>
+            <div class="columns">
+                <div class="column is-4">
+
+                </div>
+
+                <div class="column is-4 has-text-centered">
+                    <h1 id="site-name" class="title is-1">
+                        {{ config('site.name') }}
+                    </h1>
+
+                    <h2 id="site-description" class="subtitle">
+                        {{ config('site.description') }}
+                    </h2>
+                </div>
+
+                <div class="column is-4">
+
+                </div>
+            </div>
         </header>
 
         <main id="site-content">
             @yield('content')
         </main>
+
+        <footer id="site-footer" class="has-text-centered">
+            <p id="copy">&copy; {{ config('site.name') }} {{ now()->format('Y') }}</p>
+        </footer>
     </body>
 </html>
