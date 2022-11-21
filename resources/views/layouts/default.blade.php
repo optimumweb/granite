a81bfedcda563cea12c796c330bd9f9ab9f977d5
Jonathan Roy <jroy66@gmail.com>
2022-11-21 15:23:24 -0500

    add favicon support as a setting

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -12,6 +12,10 @@
             @endisset
         @endforeach
 
+        @if ($favicon = $site->theme->setting('favicon'))
+            <link rel="favicon" href="{{ $favicon }}" />
+        @endif
+
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap" rel="stylesheet">
