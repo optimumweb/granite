ef2527fb6834c63587ca000a25370b3c7c2b0132
Jonathan Roy <jroy66@gmail.com>
2023-10-02 14:43:36 -0400

    add google recaptcha support

--- resources/js/theme.js
+++ resources/js/theme.js
@@ -1,32 +1,55 @@
 window.$ = window.jQuery = require('jquery');
 
-$(document).ready(function () {
-    $('.file').each(function () {
-        let $file = $(this),
-            $fileInput = $file.find('.file-input'),
-            $fileName = $file.find('.file-name');
+$(document)
+    .ready(function () {
+        $('.file').each(function () {
+            let $file = $(this),
+                $fileInput = $file.find('.file-input'),
+                $fileName = $file.find('.file-name');
 
-        $file.removeClass('has-name');
-        $fileName.html('').hide();
+            $file.removeClass('has-name');
+            $fileName.html('').hide();
 
-        $fileInput.on('change', function () {
-            let files = $fileInput[0].files;
+            $fileInput.on('change', function () {
+                let files = $fileInput[0].files;
 
-            if (files.length > 0) {
-                let fileNames = [];
+                if (files.length > 0) {
+                    let fileNames = [];
 
-                for (let i = 0; i < files.length; i++) {
-                    let file = files[i];
+                    for (let i = 0; i < files.length; i++) {
+                        let file = files[i];
 
-                    fileNames.push(file.name);
+                        fileNames.push(file.name);
+                    }
+
+                    $file.addClass('has-name');
+                    $fileName.html(fileNames.join(', ')).show();
+                } else {
+                    $file.removeClass('has-name');
+                    $fileName.html('').hide();
                 }
-
-                $file.addClass('has-name');
-                $fileName.html(fileNames.join(', ')).show();
-            } else {
-                $file.removeClass('has-name');
-                $fileName.html('').hide();
-            }
+            });
         });
+    })
+    .on('submit', 'form.grecaptcha-enabled', function (e) {
+        let $form = $(this),
+            siteKey = $form.data('grecaptcha-site-key'),
+            token = $form.data('grecaptcha-token');
+
+        if (! token) {
+            if (siteKey) {
+                e.preventDefault();
+
+                grecaptcha.ready(function() {
+                    grecaptcha.execute(siteKey, {action: 'submit'}).then(function(token) {
+                        $form
+                            .data('grecaptcha-token', token)
+                            .append(`<input type="hidden" name="_grecaptcha_token" value="${token}" />`)
+                            .submit();
+                    });
+                });
+            } else {
+                console.log("Google reCAPTCHA Site Key not defined!");
+            }
+        }
     });
-});
--- resources/views/forms/default.blade.php
+++ resources/views/forms/default.blade.php
@@ -1,9 +1,10 @@
 <form
-    id="form-{{ $form->id }}"
-    class="form"
+    id="form-{{ $form->name }}"
+    class="form {{ $form->grecaptcha_enabled ? 'grecaptcha-enabled' : '' }}"
     action="{{ $form->action }}"
     method="{{ $form->method }}"
     enctype="multipart/form-data"
+    data-grecaptcha-site-key="{{ $form->grecaptcha_site_key }}"
 >
     @csrf
 
@@ -36,3 +37,9 @@
         {{ site()->trans('forms.submit') }}
     </button>
 </form>
+
+@if ($form->grecaptcha_enabled && isset($form->grecaptcha_site_key))
+    <!-- Google reCAPTCHA v3 -->
+    <script src="https://www.google.com/recaptcha/api.js?render={{ $form->grecaptcha_site_key }}"></script>
+    <!-- /Google reCAPTCHA v3 -->
+@endif
