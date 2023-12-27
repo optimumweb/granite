7c08004e3edbdf5e2006c2ce706fdd4d6962a1bb
Jonathan Roy <jroy66@gmail.com>
2023-12-27 13:16:48 -0500

    add author photo or gravatar

--- resources/views/post/entry.blade.php
+++ resources/views/post/entry.blade.php
@@ -32,13 +32,39 @@
             itemprop="abstract"
         >
             <div class="card-content">
-                <h3 class="title is-3">
-                    {{ site()->trans('entries.author.title') }}
-                </h3>
+                <div class="media">
+                    <div class="media-content">
+                        <h3 class="title is-3">
+                            {{ site()->trans('entries.author.title') }}
+                        </h3>
 
-                <p>
-                    {!! $authorProfile->bio !!}
-                </p>
+                        <p>
+                            {!! $authorProfile->bio !!}
+                        </p>
+                    </div>
+
+                    <div class="media-right">
+                        <figure class="image is-128x128">
+                            @isset($entry->author->photo?->url)
+                                <img
+                                    class="is-rounded"
+                                    src="{{ $entry->author->photo->url }}"
+                                    width="{{ $entry->author->photo->image_width }}"
+                                    height="{{ $entry->author->photo->image_height }}"
+                                    alt="{{ $entry->author }}"
+                                />
+                            @else
+                                <img
+                                    class="is-rounded"
+                                    src="{{ $entry->author->gravatar_url }}?s=128"
+                                    width="128"
+                                    height="128"
+                                    alt="{{ $entry->author }}"
+                                />
+                            @endisset
+                        </figure>
+                    </div>
+                </div>
             </div>
         </div>
     @endif
